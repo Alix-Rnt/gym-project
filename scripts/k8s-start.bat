77d72e834152aa0558ee807fde802384b274c858
Alix-Rnt <153102686+Alix-Rnt@users.noreply.github.com>
2026-06-23 01:27:07 +0200

    Fixed start script for Kubernetes

--- scripts/k8s-start.bat
+++ scripts/k8s-start.bat
@@ -1,1 +1,3 @@
+kubectl create configmap nginx-config --from-file=nginx.conf=./gateway/nginx.conf
+kubectl create configmap prometheus-config --from-file=prometheus.yml=./monitoring/prometheus.yml
 kubectl apply -f k8s/
